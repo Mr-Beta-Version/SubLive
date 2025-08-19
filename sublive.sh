73786d629a987993737d7f2dbdfc8ba4fc45c562
Mr. Beta <94112333+Mr-Beta-Version@users.noreply.github.com>
2025-08-19 14:36:31 +0600

    Update sublive.sh

--- sublive.sh
+++ sublive.sh
@@ -10,9 +10,9 @@ if [ -z "$domain" ]; then
 fi
 
 echo "[>] Running subfinder for $domain ..."
-subfinder -d $domain -silent | httpx -silent -status-code -no-color | tee results.txt | \
+subfinder -d $domain -silent | httpx -silent -status-code -no-color | tee all.txt | \
 awk '{gsub(/\[|\]/,"",$2); print $1 > $2".txt"}'
 
 echo "[>] Done!"
-echo " - Full result in: results.txt"
+echo " - Full result in: all.txt"
 echo " - Status code wise saved in: 200.txt, 301.txt, 302.txt, etc."
